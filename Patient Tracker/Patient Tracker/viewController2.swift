ccfe25690e1a842d0ef503845d29aea4cda11d6f
Mc-Laugh <Braden@my.prideprepschool.org>
2017-06-06 13:14:38 -0700

    Add files via upload

---
+++ Patient Tracker/Patient Tracker/viewController2.swift
@@ -0,0 +1,34 @@
+//
+//  viewController2.swift
+//  Patient Tracker
+//
+//  Created by Student on 6/6/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class viewController2: UIViewController {
+    
+   
+    @IBOutlet weak var AnswerBox: UITextField!
+    @IBOutlet weak var StatusBox: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func SubmitBox(_ sender: Any) {
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
